c5b5030a39c6ad0ae82669969e9e95bd9e57f52d
Paulo Fernando <paulo.fdo@hotmail.com>
2019-04-02 18:30:36 -0300

    Corrigido referência de modelo.

--- src/Actio.Services.Activities/Domain/Repositories/ICategoryRepository.cs
+++ src/Actio.Services.Activities/Domain/Repositories/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Actio.Services.Activities.Domain.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Actio.Services.Activities.Domain.Repositories
