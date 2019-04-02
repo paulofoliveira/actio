618aefe38187d36158cb5d3445d4acab1f6bb0ea
Paulo Fernando <paulo.fdo@hotmail.com>
2019-04-02 14:10:18 -0300

    Implementando endpoints da API.

---
+++ src/Actio.Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Actio.Common.Commands;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using System;
+using System.Threading.Tasks;
+
+namespace Actio.Api.Controllers
+{
+    [Route("[controller]/")]
+    public class ActivitiesController : Controller
+    {
+        private readonly IBusClient _busClient;
+
+        public ActivitiesController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]CreateActivity command)
+        {
+            command.Id = Guid.NewGuid();
+            command.UserId = Guid.Parse(User.Identity.Name);
+            command.CreatedAt = DateTime.UtcNow;
+
+            await _busClient.PublishAsync(command);
+
+            return Accepted($"activities/{command.Id}");
+        }
+    }
+}
---
+++ src/Actio.Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Actio.Common.Commands;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using System.Threading.Tasks;
+
+namespace Actio.Api.Controllers
+{
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IBusClient _busClient;
+
+        public UsersController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Post([FromBody]CreateUser command)
+        {
+            await _busClient.PublishAsync(command);
+            return Accepted();
+        }
+    }
+}
