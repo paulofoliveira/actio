8da5ddf51331ae41190de0d2fa93772fc82950dc
Paulo Fernando <paulo.fdo@hotmail.com>
2019-04-02 23:24:04 -0300

    Acertos para registro de usuário.

--- src/Actio.Services.Identity/Startup.cs
+++ src/Actio.Services.Identity/Startup.cs
@@ -1,12 +1,17 @@
-﻿using Actio.Common.Mongo;
+﻿using Actio.Common.Commands;
+using Actio.Common.Mongo;
+using Actio.Common.RabbitMq;
 using Actio.Services.Identity.Domain.Repositories;
 using Actio.Services.Identity.Domain.Services;
+using Actio.Services.Identity.Handlers;
 using Actio.Services.Identity.Repositories;
+using Actio.Services.Identity.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Actio.Services.Identity
 {
@@ -19,14 +24,20 @@ namespace Actio.Services.Identity
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddMongoDB(Configuration);
+            services.AddRabbitMq(Configuration);
+            services.AddScoped<ICommandHandler<CreateUser>, CreateUserHandler>();
 
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddSingleton<IEncrypter, Encrypter>();
+
+            services.AddScoped<IUserService, UserService>();
+
+            return services.BuildServiceProvider();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
