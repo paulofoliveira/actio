fef12ad5c8e4c67c0d04692a138c276e8ea0a504
Paulo Fernando <paulo.fdo@hotmail.com>
2019-04-03 15:03:13 -0300

    Refatorado Endpoint de Activity.

--- src/Actio.Api/Controllers/ActivitiesController.cs
+++ src/Actio.Api/Controllers/ActivitiesController.cs
@@ -1,28 +1,51 @@
-﻿using Actio.Common.Commands;
+﻿using Actio.Api.Repositories;
+using Actio.Common.Commands;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RawRabbit;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Actio.Api.Controllers
 {
     [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ActivitiesController : Controller
     {
         private readonly IBusClient _busClient;
+        private readonly IActivityRepository _repository;
 
-        public ActivitiesController(IBusClient busClient)
+        public ActivitiesController(IBusClient busClient, IActivityRepository repository)
         {
             _busClient = busClient;
+            _repository = repository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var activities = await _repository.BrowseAsync(UserId);
+            return Json(activities.Select(x => new { x.Id, x.Name, x.Category, x.CreatedAt }));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var activity = await _repository.GetAsync(id);
+            if (activity == null) return NotFound();
+
+            if (activity.UserId != UserId) return Unauthorized();
+
+            return Json(activity);
         }
 
         [HttpPost("")]
         public async Task<IActionResult> Post([FromBody]CreateActivity command)
         {
             command.Id = Guid.NewGuid();
-            command.UserId = Guid.Parse(User.Identity.Name);
+            command.UserId = UserId;
             command.CreatedAt = DateTime.UtcNow;
 
             await _busClient.PublishAsync(command);
@@ -30,8 +53,6 @@ namespace Actio.Api.Controllers
             return Accepted($"activities/{command.Id}");
         }
 
-        [HttpGet]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public IActionResult Get() => Content("Secured");
+        public Guid UserId { get => Guid.Parse(User.Identity.Name); }
     }
 }
