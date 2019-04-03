18fdab2780e38c064f4f9166534a3b827ec35432
Paulo Fernando <paulo.fdo@hotmail.com>
2019-04-03 17:38:49 -0300

    Testes em Service API.

--- src/Actio.Services.Activities/Controllers/HomeController.cs
+++ src/Actio.Services.Activities/Controllers/HomeController.cs
@@ -6,6 +6,6 @@ namespace Actio.Services.Activities.Controllers
     public class HomeController : Controller
     {
         [HttpGet("")]
-        public IActionResult Get() => Content("Hello from Actio Services Activities API!");
+        public IActionResult Get() => Content("Hello from Actio.Services.Activities API!");
     }
 }
